8af005e9422f7aecd0299cf4a5037d3376df40f6
Alex Milevski <alex@milevski.co>
2025-03-17 23:10:51 +0100

    Added coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,13 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     setupFiles: ["./src/__tests__/setup.ts"],
+    coverage: {
+      reporter: ["json", "cobertura"],
+      include: ["src/**/*.{ts,tsx}"],
+      all: true,
+      reportsDirectory: "reports/coverage",
+    },
+    reporters: ["default", "junit"],
+    outputFile: "reports/unit/junit-test-results.xml",
   },
 });
